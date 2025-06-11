13c83eb8b8d2ff6490042885fe4aad822da4940f
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2025-06-11 20:32:50 +0700

    use IndexDB fix much request

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,51 +1,69 @@
-import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
 
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
         }
 
-        if (args.path.includes('./') || args.path.includes('../')) {
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            namespace: 'a',
+            namespace: "a",
             path: new URL(
               args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
+              "https://unpkg.com" + args.resolveDir + "/"
             ).href,
           };
         }
 
         return {
-          namespace: 'a',
+          namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+        console.log("onLoad", args);
 
-        if (args.path === 'index.js') {
+        if (args.path === "index.js") {
           return {
-            loader: 'jsx',
+            loader: "jsx",
             contents: `
-              import React, { useState } from 'react';
+              import React, { useState } from 'react-select';
               console.log(React, useState);
             `,
           };
         }
 
+        // Check to see if we have already fetched this file
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem(args.path);
+
+        // if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
         const { data, request } = await axios.get(args.path);
-        return {
-          loader: 'jsx',
+
+        const result = {
+          loader: "jsx",
           contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
