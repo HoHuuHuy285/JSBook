ad63d4245d1a690de984c1f081b3e6a3b6fe5b67
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2025-06-15 09:48:13 +0700

    Defining Direction Type

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,12 +1,13 @@
-import { ActionType } from '../action-types';
+import { ActionType } from "../action-types";
 import {
   Action,
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
   InsertCellBeforeAction,
-} from '../actions';
-import { CellTypes } from '../cell';
+  Direction,
+} from "../actions";
+import { CellTypes } from "../cell";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -25,10 +26,7 @@ export const deleteCell = (id: string): DeleteCellAction => {
   };
 };
 
-export const moveCell = (
-  id: string,
-  direction: 'up' | 'down'
-): MoveCellAction => {
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   return {
     type: ActionType.MOVE_CELL,
     payload: {
