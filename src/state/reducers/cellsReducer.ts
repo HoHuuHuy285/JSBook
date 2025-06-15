201a79a34c96d0130780a4a05d8ea9925d514b76
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2025-06-15 15:40:25 +0700

    Swapping Cell Placement

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,7 +1,7 @@
-import produce from "immer";
-import { ActionType } from "../action-types";
-import { Action } from "../actions";
-import { Cell } from "../cell";
+import produce from 'immer';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+import { Cell } from '../cell';
 
 interface CellsState {
   loading: boolean;
@@ -32,7 +32,18 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
 
       return;
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
+      return;
     case ActionType.INSERT_CELL_BEFORE:
       return state;
     default:
