ac75b01f5a7fd1891c89f36348c5d434bf3d9149
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2025-06-16 14:31:35 +0700

    Apply custom CSS

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,5 +1,5 @@
-import { useDispatch } from "react-redux";
-import { Cell, actionCreators } from "../state";
+import "./cell-list-item.css";
+import { Cell } from "../state";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
 import ActionBar from "./action-bar";
@@ -11,17 +11,24 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === "code") {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
   }
 
-  return (
-    <div>
-      <ActionBar id={cell.id} />
-      {child}
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
