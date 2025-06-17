cf1e0a69d03ff5b92626d4c79456df430b8d1cbc
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2025-06-17 14:35:16 +0700

    Implementing a show Function no Op

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,11 +1,11 @@
-import './code-cell.css';
-import { useEffect } from 'react';
-import CodeEditor from './code-editor';
-import Preview from './preview';
-import Resizable from './resizable';
-import { Cell } from '../state';
-import { useActions } from '../hooks/use-actions';
-import { useTypedSelector } from '../hooks/use-typed-selector';
+import "./code-cell.css";
+import { useEffect } from "react";
+import CodeEditor from "./code-editor";
+import Preview from "./preview";
+import Resizable from "./resizable";
+import { Cell } from "../state";
+import { useActions } from "../hooks/use-actions";
+import { useTypedSelector } from "../hooks/use-typed-selector";
 
 interface CodeCellProps {
   cell: Cell;
@@ -18,27 +18,32 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [
-      `
-        import _React from 'react';
-        import _ReactDOM from 'react-dom';
-        const show = (value) => {
-          const root = document.querySelector('#root');
+    const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+    var show = (value) => {
+      const root = document.querySelector('#root');
 
-          if (typeof value === 'object') {
-            if (value.$$typeof && value.props) {
-              _ReactDOM.render(value, root);
-            } else {
-              root.innerHTML = JSON.stringify(value);
-            }
-          } else {
-            root.innerHTML = value;
-          }
-        };
-      `,
-    ];
+      if (typeof value === 'object') {
+        if (value.$$typeof && value.props) {
+          _ReactDOM.render(value, root);
+        } else {
+          root.innerHTML = JSON.stringify(value);
+        }
+      } else {
+        root.innerHTML = value;
+      }
+    };
+  `;
+    const showFuncNoop = " var show = () => {}";
+    const cumulativeCode = [];
     for (let c of orderedCells) {
-      if (c.type === 'code') {
+      if (c.type === "code") {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
@@ -50,27 +55,27 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cumulativeCode.join('\n'));
+      createBundle(cell.id, cumulativeCode.join("\n"));
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cumulativeCode.join('\n'));
+      createBundle(cell.id, cumulativeCode.join("\n"));
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cumulativeCode.join('\n'), cell.id, createBundle]);
+  }, [cumulativeCode.join("\n"), cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
       <div
         style={{
-          height: 'calc(100% - 10px)',
-          display: 'flex',
-          flexDirection: 'row',
+          height: "calc(100% - 10px)",
+          display: "flex",
+          flexDirection: "row",
         }}
       >
         <Resizable direction="horizontal">
